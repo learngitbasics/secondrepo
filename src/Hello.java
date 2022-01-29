380275fea7d16b76dff626e0af305f41fa21bd0c
learngitbasics <learngitbasics@gmail.com>
2022-01-29 18:22:05 +0530

    Hello changed

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("sc");
+        System.out.println("Great learning");
     }
 }
